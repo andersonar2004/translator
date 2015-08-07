1ace5b7d3fb9373adba35c84da417874d1508f1b
Anderson Rodrigues <anderson_ar2004@hotmail.com>
2015-08-07 16:22:49 -0300

    limpando o código

--- bing_translator.rb
+++ bing_translator.rb
@@ -3,7 +3,10 @@ require 'headless'
 require 'watir-webdriver'
 
 url  = 'http://www.bing.com/translator/default.aspx?MKT=pt-br'
-#browser = Watir::Browser.new :chrome
+language_out = 'pt' # 'en' for english
+# origin_text = "Como vai você meu caro?"
+origin_text = "How are you my dear?"
+
 headless = Headless.new
 headless.start
 browser = Watir::Browser.new
@@ -15,22 +18,19 @@ button = browser.div(:id => "TranslateButton")
 loading = browser.div(:id => "ProgressDiv")
 
 # Select language out
-language = 'pt' # 'en' for english
 browser.span(:id=>"Header_DstLangList").click
-browser.link(:id=>"Lang_DstLangList_#{language}").when_present {|link| link.click}
+browser.link(:id=>"Lang_DstLangList_#{language_out}").when_present {|link| link.click}
 
+### debugs
+# puts "input encontrado #{input.exist?}"
+# puts "output encontrado #{output.exist?}"
+# puts "button encontrado #{button.exist?}"
 
-puts "input encontrado #{input.exist?}"
-puts "output encontrado #{output.exist?}"
-puts "button encontrado #{button.exist?}"
-
-# origin_text = "Como vai você meu caro?"
-origin_text = "How are you my dear?"
-
-
+# Efetua comandos
 input.set origin_text
 input.fire_event :change
 button.fire_event :click
+# Aguarda o retorno ajax
 loading.wait_while_present
 
 puts "#{origin_text} => #{output.text}\n"
